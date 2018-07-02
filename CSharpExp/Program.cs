5e993611b7bdab0b5237d93de0d920ad7c3741f5
Fly <954455187@qq.com>
2018-07-02 15:47:36 +0800

    init pro

---
+++ CSharpExp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;//使用系统命名空间
+
+namespace CSharpExp //自定义命名空间
+{
+    class Program//类名
+    {
+        static void Main(string[] args)//同java，主函数，程序入口
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
