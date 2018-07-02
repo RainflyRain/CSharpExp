9d726fb7ebc6fca02b0fd3535a6c0e70e157bb6d
Fly <954455187@qq.com>
2018-07-02 16:36:30 +0800

    basic c#

--- CSharpExp/Program.cs
+++ CSharpExp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;//使用系统命名空间
+using RectangleApplication;//使用自定义命名空间
 
 namespace CSharpExp //自定义命名空间
 {
@@ -6,7 +7,20 @@ namespace CSharpExp //自定义命名空间
     {
         static void Main(string[] args)//同java，主函数，程序入口
         {
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Acceptdetails();
+            rectangle.Display();
             Console.WriteLine("Hello World!");
         }
     }
 }
+
+/*
+*
+* 标识符：类名 方法名 成员变量名
+* 
+* 类名：大写开头 驼峰
+* 方法名：大写开头 驼峰
+* 成员变量：小写
+*/
---
+++ CSharpExp/RectangleExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RectangleApplication//自定义命名空间
+{
+
+    class Rectangle
+    {
+
+        //成员变量
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+
+        public double GetArea()
+        {
+            return width * length;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length:{0}", length);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Area:{0}", GetArea());
+        }
+    }
+
+}
